666c1ceb0d5d693381aa9f53452c0eceeb298631
raffaez <rafaeleasouzas@gmail.com>
2022-11-25 21:45:34 -0300

    feat: navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from './components/static/navbar/Navbar';
 import Home from './pages/home/Home';
+import Footer from './components/static/footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
